22af1ac8ceb646e4ae71bdcbd5000e67bef2352a
Igor <igor_paiva21@live.com>
2021-06-03 13:35:07 -0300

    task1 finished

--- task1.py
+++ task1.py
@@ -4,15 +4,11 @@ def solution(S):
     if size == 0:
         pass
     for i in range(0, (((size) + 1) // 2)):
-        #print("i: ", S[i], " and after half: ", S[(size - i )])
         if S[i] != S[size - i]:
             if S[i] == "?" and S[size - i] != "?":
-                print("primeiro")
                 S = S[0:i] + S[size - i] + S[i+1:]
             elif S[i] != "?" and S[size - i] == "?":
-                print("segundo: ", S[size])
                 S = S[0:(size - i)] + S[i] + S[(size - i) +1:]
-                #S = S.replace(S[size - i], S[i], 1)
             else:
                 #print("NO")
                 return "NO"
